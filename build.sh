09ba5288f7a16e22902c170487084f32684c2907
Wojciech M. Zabołotny <wzab01@gmail.com>
2019-07-29 14:24:21 +0200

    Ported do Vivado 2019.1.1.

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
-git submodule update --init --recursive
+(
+  cd original/j1b/verilator
+  ./localtest_afck_gen_prog
+)
+(
+  cd src/j1b
+  ./ram_init.py mem_dump.hex
+)
 vivado -mode batch -source eprj_create.tcl
 vivado -mode batch -source eprj_write.tcl
 vivado -mode batch -source eprj_build.tcl
