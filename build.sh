672feb6ca363b5b4d788242765ff44032db94c6e
Wojciech M. Zabołotny <wzab01@gmail.com>
2019-07-29 14:09:20 +0200

    Initial port to Vivado 2019.1.1

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+git submodule update --init --recursive
 vivado -mode batch -source eprj_create.tcl
+vivado -mode batch -source eprj_write.tcl
 vivado -mode batch -source eprj_build.tcl
 
